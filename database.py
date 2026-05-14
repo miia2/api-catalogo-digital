08cf76e21f1846f941fa8dd83a441fbffe36f4fa
Mirian <miriansoaresg85@gmail.com>
2026-05-14 18:07:24 -0300

    Fix: Adicionando Base ao database.py

--- database.py
+++ database.py
@@ -1,14 +1,12 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base # <-- Adicionado aqui
+from sqlalchemy.orm import sessionmaker, declarative_base # Adicionamos o declarative_base aqui
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Pega o link do banco do .env. Se não achar, usa o SQLite de backup para testes
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./catalogo.db")
 
-# O SQLite precisa de "check_same_thread", o Postgres não pode ter isso.
 if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
     engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 else:
@@ -16,8 +14,8 @@ else:
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# A BASE QUE ESTAVA FALTANDO!
-Base = declarative_base()
+# ESSA LINHA ABAIXO É A QUE ESTÁ FALTANDO:
+Base = declarative_base() 
 
 def get_db():
     db = SessionLocal()
