89932ad60a4a024b38fc46bb457948d2730e52ca
Mirian <miriansoaresg85@gmail.com>
2026-05-23 11:59:25 -0300

    Consertando o link

--- database.py
+++ database.py
@@ -14,9 +14,9 @@ if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://")
 
 # Se não houver URL (testes locais), usa o SQLite
 if not SQLALCHEMY_DATABASE_URL:
-    SQLALCHEMY_DATABASE_URL = "sqlite:///./catalogo.db"
+    SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:npg_2We9KjAgQUDr@ep-wandering-recipe-aq7h1dfq-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./catalogo.db")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://neondb_owner:npg_2We9KjAgQUDr@ep-wandering-recipe-aq7h1dfq-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 
 if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
     engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
