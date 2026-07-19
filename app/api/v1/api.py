22f925f2e81aef0668fc69a4241be117745bdcf7
Mirian <miriansoaresg85@gmail.com>
2026-07-19 15:56:49 -0300

    corrigindo a api

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth, users, products
+from app.api.v1.endpoints import auth, products
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["Autenticação"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(products.router, prefix="/products", tags=["Produtos & Lojas"])
